1758e0ffd41d43a7c674f83c4e9607202ca96cf1
CatsAreMLG <joshuadgon@gmail.com>
2019-05-14 22:03:23 -0500

    Add postgres database from heroku

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,8 @@
-require('dotenv').config(); 
-const pg = require('pg');
-pg.defaults.ssl = true;
+require('dotenv').config()
+const pg = require('pg')
+pg.defaults.ssl = true
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
@@ -11,24 +10,21 @@ module.exports = {
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './database/migrations',
+      directory: './database/migrations'
     },
     seeds: {
-      directory: './database/seeds',
-    },
+      directory: './database/seeds'
+    }
   },
 
   production: {
     client: 'pg',
-    connection: process.env.databaseBASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './database/migrations',
-      tableName: 'knex_migrations'
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   }
-
-};
+}
