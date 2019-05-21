c50730bba8d4f8fb6926ad690a25ed904efef29e
Kseniya Platonava <ksu2008@gmail.com>
2019-05-20 23:18:29 -0400

    Add Market-router.js
    add get market
    add get market by ID

---
+++ database/routes/markets-router.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const jwt = require('jsonwebtoken')
+const Markets = require('../helpers/marketsHelper')
+
+router.get('/', async (req, res) => {
+  try {
+    const markets = await Markets.getMarkets()
+    res.status(200).json(markets)
+  } catch (error) {
+    res.status(500).json({ error })
+  }
+})
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const market = await Markets.getMarketById(id)
+    market
+      ? res.status(200).json(market)
+      : res.status(404).json({ error: 'Market is not found' })
+  } catch (error) {
+    res.status(500).json({ error })
+  }
+})
+
+module.exports = router
