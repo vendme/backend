f60ae499238b525bd4e6740bf98283b5b9b3ec2d
Lavon Stalling <micah.shrak@gmail.com>
2019-05-23 19:48:55 -0700

    Seeded products table

---
+++ data/seeds/011-products_seeds.js
@@ -0,0 +1,26 @@
+const faker = require('faker');
+
+const createFakeProducts = () => {
+	let fakeProducts = [];
+	const desiredFakeProducts = 100;
+	for (let i = 0; i < desiredFakeProducts; i++) {
+		fakeProducts.push({
+			id: i + 1,
+			product_name: faker.commerce.productName(),
+			product_description: faker.lorem.words(),
+			product_price: faker.commerce.price(),
+			product_category: faker.random.number({ min: 1, max: 10 }),
+			product_image: faker.image.image(),
+		});
+	}
+	return fakeProducts;
+};
+
+exports.seed = function(knex, Promis) {
+	return knex('products')
+		.del()
+		.then(function() {
+			// Inserts seed entries
+			return knex('products').insert(createFakeProducts());
+		});
+};
