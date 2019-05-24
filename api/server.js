1454297aa25f5d161b90a9e02b44f6d02999af89
CatsAreMLG <joshuadgon@gmail.com>
2019-05-24 17:36:10 -0500

    Fix local database ssl error

--- api/server.js
+++ api/server.js
@@ -1,52 +1,52 @@
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
-const session = require('express-session');
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+const session = require('express-session')
 const KnexSessionStore = require('connect-session-knex')(session)
-const passport = require('passport');
-require('../config/passport-setup');
-require('dotenv').config();
+const passport = require('passport')
+require('../config/passport-setup')
+require('dotenv').config()
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../database/routes/users-router.js');
-const vendorRouter = require('../database/routes/vendors-router.js');
-const marketRouter = require('../database/routes/markets-router.js');
+const authRouter = require('../auth/auth-router.js')
+const usersRouter = require('../database/routes/users-router.js')
+const vendorRouter = require('../database/routes/vendors-router.js')
+const marketRouter = require('../database/routes/markets-router.js')
 
-const server = express();
+const server = express()
 
 const sessionOptions = {
   name: 'vendme',
   secret: process.env.COOKIE_KEY,
   cookie: {
-      maxAge: 1000 * 60 * 60, // 1 hour
-      secure: false
+    maxAge: 1000 * 60 * 60, // 1 hour
+    secure: false
   },
   httpOnly: true,
   resave: false,
   saveUninitialized: false,
   store: new KnexSessionStore({
-      knex: require('../database/dbConfig'),
-      tablename: 'sessions',
-      sidfieldname: 'sid',
-      createtable: true,
-      clearInterval: 1000 * 60 * 60 // hour
+    knex: require('../database/dbConfig.js'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60 // hour
   })
 }
 
-server.use(session(sessionOptions));
-server.use(passport.initialize());
-server.use(passport.session());
-server.use(express.json());
-server.use(helmet());
-server.use(cors());
+// server.use(session(sessionOptions));
+server.use(passport.initialize())
+server.use(passport.session())
+server.use(express.json())
+server.use(helmet())
+server.use(cors())
 
 server.use('/api/users', usersRouter)
 server.use('/api/vendor', vendorRouter)
 server.use('/api/market', marketRouter)
 server.use('/', authRouter)
 
-server.get("/", (req, res) => {
-  res.send("I am the Vendme backend server. Up and running!");
-});
+server.get('/', (req, res) => {
+  res.send('I am the Vendme backend server. Up and running!')
+})
 
-module.exports = server;
+module.exports = server
--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,8 +1,8 @@
-const knex = require('knex');
+const knex = require('knex')
 
-const knexConfig = require('../knexfile.js');
+const knexConfig = require('../knexfile.js')
 
-const databaseEngine = process.env.DABTABASE_URL || 'development';
+const databaseEngine = process.env.DABTABASE_URL || 'development'
 
-// module.exports = knex(knexConfig.development);
-module.exports = knex(knexConfig[databaseEngine]);
+// module.exports = knex(knexConfig.development)
+module.exports = knex(knexConfig[databaseEngine])
