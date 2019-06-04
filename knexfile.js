8509b17c53446b48f7960b4325bf7eeca2d2cf55
Brett Madrid <brettmadrid9@gmail.com>
2019-06-03 19:36:30 -0700

    Backend SQLite3 refresh

--- knexfile.js
+++ knexfile.js
@@ -1,102 +1,6 @@
-require('dotenv').config();
-const pg = require('pg');
-
-const localPostgres = {
-	host: process.env.HOST,
-	database: process.env.DATABASE,
-	port: process.env.PORT,
-	user: process.env.USER,
-	password: process.env.PASS || '',
-};
-
-const dbConnection = process.env.DATABASE_URL || localPostgres;
-
-const dbLocalSettings = {
-	client: 'pg',
-	connection: dbConnection,
-	pool: {
-		min: 2,
-		max: 10,
-	},
-	migrations: {
-		directory: './database/migrations',
-		tableName: 'vendme_migrations',
-	},
-	seeds: {
-		directory: './database/seeds/development',
-	},
-};
-
-const dbSqliteDev = {
-	client: 'sqlite3',
-	connection: {
-		filename: './database/vendme.db3',
-	},
-	useNullAsDefault: true,
-	migrations: {
-		directory: './database/migrations',
-	},
-	seeds: {
-		directory: './database/seeds/sqlite',
-	},
-};
-const dbSettings = {
-	client: 'pg',
-	connection: dbConnection,
-	pool: {
-		min: 2,
-		max: 10,
-	},
-	migrations: {
-		directory: './database/migrations',
-		tableName: 'vendmeprod_migrations',
-	},
-	seeds: {
-		directory: './database/seeds/production',
-	},
-};
+require('dotenv').config(); 
 
 module.exports = {
-	development: dbLocalSettings, //dbSqliteDev,
-	production: dbSettings,
-};
-
-/* require('dotenv').config();
-//const pg = require('pg');
-//pg.defaults.ssl = true;
-
-const localPostgres = {
-	host: process.env.HOST,
-	database: process.env.DATABASE,
-	port: process.env.PORT,
-	user: process.env.USER,
-	password: process.env.PASS || '',
-};
-
-const dbConnection = process.env.DATABASE_URL || localPostgres;
-
-const dbSettings = {
-	client: 'pg',
-	connection: dbConnection,
-	pool: {
-		min: 2,
-		max: 10,
-	},
-	migrations: {
-		directory: './database/migrations',
-		tableName: 'vendme_migrations',
-	},
-	seeds: {
-		directory: './data/seeds',
-	},
-};
-
-module.exports = {
-	development: dbLocalSettings,
-	production: dbSettings,
-}; */
-
-/* module.exports = {
 	development: {
 		client: 'sqlite3',
 		connection: {
@@ -107,18 +11,7 @@ module.exports = {
 			directory: './database/migrations',
 		},
 		seeds: {
-			directory: './database/seeds',
-		},
-	},
-
-	production: {
-		client: 'pg',
-		connection: process.env.DATABASE_URL,
-		migrations: {
-			directory: './database/migrations',
-		},
-		seeds: {
 			directory: './data/seeds',
 		},
 	},
-}; */
+}; 
