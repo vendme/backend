fd024ea193b417359d15f85d6b9b469b5350cb44
Brett Madrid <brettmadrid9@gmail.com>
2019-06-04 19:42:46 -0700

    initial commit

--- knexfile.js
+++ knexfile.js
@@ -1,31 +1,31 @@
 require('dotenv').config();
-const pg = require('pg');
+// const pg = require('pg');
 
-const localPostgres = {
-	host: process.env.HOST,
-	database: process.env.DATABASE,
-	port: process.env.PORT,
-	user: process.env.USER,
-	password: process.env.PASS || '',
-};
+// const localPostgres = {
+// 	host: process.env.HOST,
+// 	database: process.env.DATABASE,
+// 	port: process.env.PORT,
+// 	user: process.env.USER,
+// 	password: process.env.PASS || '',
+// };
 
-const dbConnection = process.env.DATABASE_URL || localPostgres;
+// const dbConnection = process.env.DATABASE_URL || localPostgres;
 
-const dbLocalSettings = {
-	client: 'pg',
-	connection: dbConnection,
-	pool: {
-		min: 2,
-		max: 10,
-	},
-	migrations: {
-		directory: './database/migrations',
-		tableName: 'vendme_migrations',
-	},
-	seeds: {
-		directory: './database/seeds/development',
-	},
-};
+// const dbLocalSettings = {
+// 	client: 'pg',
+// 	connection: dbConnection,
+// 	pool: {
+// 		min: 2,
+// 		max: 10,
+// 	},
+// 	migrations: {
+// 		directory: './database/migrations',
+// 		tableName: 'vendme_migrations',
+// 	},
+// 	seeds: {
+// 		directory: './database/seeds/development',
+// 	},
+// };
 
 const dbSqliteDev = {
 	client: 'sqlite3',
@@ -40,25 +40,26 @@ const dbSqliteDev = {
 		directory: './database/seeds/sqlite',
 	},
 };
-const dbSettings = {
-	client: 'pg',
-	connection: dbConnection,
-	pool: {
-		min: 2,
-		max: 10,
-	},
-	migrations: {
-		directory: './database/migrations',
-		tableName: 'vendmeprod_migrations',
-	},
-	seeds: {
-		directory: './database/seeds/production',
-	},
-};
+
+// const dbSettings = {
+// 	client: 'pg',
+// 	connection: dbConnection,
+// 	pool: {
+// 		min: 2,
+// 		max: 10,
+// 	},
+// 	migrations: {
+// 		directory: './database/migrations',
+// 		tableName: 'vendmeprod_migrations',
+// 	},
+// 	seeds: {
+// 		directory: './database/seeds/production',
+// 	},
+// };
 
 module.exports = {
-	development: dbLocalSettings,
-	production: dbSettings,
+	development: dbSqliteDev,
+	//production: dbSettings,
 };
 
 /* require('dotenv').config();
