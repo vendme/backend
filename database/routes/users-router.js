b5fd199de9903eb0afc6ba60512fd21d08c16583
Kseniyapl <kplatona@Kseniyas-MacBook-Pro.local>
2019-06-05 18:11:15 -0400

    testing users get by id

--- database/routes/users-router.js
+++ database/routes/users-router.js
@@ -9,5 +9,46 @@ router.get('/', (req, res) => {
     })
     .catch(err => res.send(err));
 });
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const user = await Users.findById(id)
+    user
+      ? res.status(200).json(user)
+      : res.status(404).json({ error: 'user is not found' })
+  } catch (error) {
+    res.status(507).json({ error })
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const user = await Users.add(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error adding the user',
+    });
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const count = await Users.deleteUser(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ message: 'The user has been deleted' });
+    } else {
+      res.status(404).json({ message: 'The user could not be found' });
+    }
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error removing the user',
+    });
+  }
+})
+
 
 module.exports = router;
