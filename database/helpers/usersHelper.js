a1c473c225eacc96cd34d9b66f89ea7f7ae3706a
Lavon Stalling <micah.shrak@gmail.com>
2019-06-05 20:14:24 -0700

    Update knexfile.js

--- database/helpers/usersHelper.js
+++ database/helpers/usersHelper.js
@@ -1,31 +1,34 @@
 const db = require('../dbConfig.js');
 
 module.exports = {
-  add,
-  find,
-  findBy,
-  findById,
-  findByAccountType
+	add,
+	find,
+	findBy,
+	findById,
+	findByAccountType,
 };
 
 function find() {
-  return db('users').select('id', 'username', 'password');
+	return db('users').select('id', 'username', 'password');
 }
 
 function findBy(filter) {
-  return db('users').where(filter);
+	return db('users').where(filter);
 }
 
-async function add(user) {
-  const [id] = await db('users').insert(user);
+function add(user) {
+	//const [id] =
+	return db('users')
+		.insert(user)
+		.returning(['id']);
 
-  return findById(id);
+	//return findById(id);
 }
 
 function findById(id) {
-  return db('users')
-    .where({ id })
-    .first();
+	return db('users')
+		.where({ id })
+		.first();
 }
 function findByAccountType(account_type) {
 	return db('users').where({ account_type });
--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+// Comment out if using sqlite3 local and uncomment bottom code
+
 require('dotenv').config();
 const pg = require('pg');
 
@@ -61,64 +63,49 @@ module.exports = {
 	production: dbSettings,
 };
 
-/* require('dotenv').config();
-//const pg = require('pg');
-//pg.defaults.ssl = true;
+// Uncomment if for sqlite3 local
+
+/* require('dotenv').config()
+const pg = require('pg')
+pg.defaults.ssl = true
 
 const localPostgres = {
-	host: process.env.HOST,
-	database: process.env.DATABASE,
-	port: process.env.PORT,
-	user: process.env.USER,
-	password: process.env.PASS || '',
-};
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  port: process.env.PORT,
+  user: process.env.USER,
+  password: process.env.PASS || ''
+}
 
-const dbConnection = process.env.DATABASE_URL || localPostgres;
+const dbConnection = localPostgres
 
 const dbSettings = {
-	client: 'pg',
-	connection: dbConnection,
-	pool: {
-		min: 2,
-		max: 10,
-	},
-	migrations: {
-		directory: './database/migrations',
-		tableName: 'vendme_migrations',
-	},
-	seeds: {
-		directory: './data/seeds',
-	},
-};
+  client: 'pg',
+  connection: dbConnection,
+  migrations: {
+    directory: './database/migrations',
+    tableName: 'vendme_migrations'
+  },
+  seeds: {
+    directory: './data/seeds'
+  }
+}
 
 module.exports = {
-	development: dbLocalSettings,
-	production: dbSettings,
-}; */
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/vendme.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
+  },
 
-/* module.exports = {
-	development: {
-		client: 'sqlite3',
-		connection: {
-			filename: './database/vendme.db3',
-		},
-		useNullAsDefault: true,
-		migrations: {
-			directory: './database/migrations',
-		},
-		seeds: {
-			directory: './database/seeds',
-		},
-	},
-
-	production: {
-		client: 'pg',
-		connection: process.env.DATABASE_URL,
-		migrations: {
-			directory: './database/migrations',
-		},
-		seeds: {
-			directory: './data/seeds',
-		},
-	},
-}; */
+  production: dbSettings
+}
+ */
