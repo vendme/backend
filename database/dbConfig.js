6f6e8f1d23b7f5a0b4bcd55b51bdc7ead2063f9e
Lavon Stalling <micah.shrak@gmail.com>
2019-06-19 19:13:33 -0700

    update

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -3,7 +3,7 @@ const knex = require('knex');
 
 const knexConfig = require('../knexfile.js');
 
-const databaseEngine = process.env.DB || 'development';
+const databaseEngine = 'development';
 //const databaseEngine = 'development';
 
 // module.exports = knex(knexConfig.development);
