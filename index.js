9ca341b522890010357f97b366e81325971a49cc
brettmadrid <brettmadrid9@gmail.com>
2019-06-19 20:04:36 -0700

    knexfile updates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config(); // load .env variables
-
+console.log('ENV Variables', process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.COOKIE_KEY, process.env.DATABASE_ENV)
 const server = require('./api/server.js');
 
 const port = process.env.PORT || 9000;
