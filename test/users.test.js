c8fe94a82278a35af1ca143475e7b2cdb0a928a7
kplatona <ksu2008@gmail.com>
2019-06-20 23:30:03 -0400

    Start to work on user test

---
+++ test/users.test.js
@@ -0,0 +1,12 @@
+const knex = require('../database/dbConfig');
+const admin = require('../firebase-admin/admin');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiHttp = require('chai-http');
+const app = require('../api/server');
+const rp = require('request-promise');
+const fixtures = require('./fixtures');
+
+
+
+
