8d10225e263e03ef4bcb918d00c9d1ec1dc2584b
Lavon Stalling <micah.shrak@gmail.com>
2019-06-26 23:04:45 -0700

    changed date_open in dev to days_open

--- database/seeds/development/005-markets_seeds.js
+++ database/seeds/development/005-markets_seeds.js
@@ -12,7 +12,7 @@ const createFakeMarkets = () => {
 			state: faker.address.stateAbbr(),
 			zip_code: faker.address.zipCode(),
 			phone_num: faker.phone.phoneNumberFormat(),
-			date_open: faker.date.weekday(),
+			days_open: faker.date.weekday(),
 			hours_open: '9AM - 8PM',
 			market_info: faker.lorem.paragraph(),
 			market_map_file: faker.image.image(),
