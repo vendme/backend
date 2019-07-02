04756fa4a09593b0bbbbd558a845cc796822e3d9
CatsAreMLG <joshuadgon@gmail.com>
2019-07-02 12:52:28 -0500

    Change market fields

--- database/migrations/20190602133645_create_markets_table.js
+++ database/migrations/20190602133645_create_markets_table.js
@@ -1,51 +1,38 @@
 exports.up = function(knex, Promise) {
-	return knex.schema.createTable('markets', function(tbl) {
-		// PK
-		tbl.increments();
+  return knex.schema.createTable('markets', function(tbl) {
+    // PK
+    tbl.increments()
 
-		// Markets Info
-		tbl.string('market_name', 500).notNullable();
+    // Markets Info
+    tbl.string('market_name', 500).notNullable()
 
-		tbl.string('address', 255).notNullable();
+    tbl.string('address', 255).notNullable()
 
-		tbl.string('city', 255).notNullable();
+    tbl.string('city', 255).notNullable()
 
-		tbl.string('state', 2).notNullable();
+    tbl.string('state', 2).notNullable()
 
-		tbl.string('zip_code', 10).notNullable();
+    tbl.string('zip_code', 10).notNullable()
 
-		tbl.string('phone_num', 20).notNullable();
+    tbl.string('phone_num', 20).notNullable()
 
-		tbl.string('market_info', 1000).notNullable();
+    tbl.string('market_info', 1000).notNullable()
 
-		tbl.string('days_open', 255);
+    tbl.string('hours_open', 70)
 
-		tbl.string('hours_open', 255);
+    tbl.string('market_map_file', 2048)
 
-		tbl.string('market_map_file', 2048);
+    tbl.string('agreement_file', 2048)
 
-		tbl.bigInteger('created_at').notNullable();
+    tbl.bigInteger('created_at').notNullable()
 
-		//tbl.binary('market_map_file', 2048);
-
-		//FK
-		/* tbl
-			.integer('days_open')
-			.references('id')
-			.inTable('days'); */
-		//FK
-		/* tbl
-			.integer('hours_of_operation')
-			.references('id')
-			.inTable('hours'); */
-
-		tbl
-			.integer('user_market')
-			.references('id')
-			.inTable('users');
-	});
-};
+    tbl
+      .integer('user_market')
+      .references('id')
+      .inTable('users')
+  })
+}
 
 exports.down = function(knex, Promise) {
-	return knex.schema.dropTableIfExists('markets');
-};
+  return knex.schema.dropTableIfExists('markets')
+}
