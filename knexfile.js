09b74031e4809e07dfd6b92d60c3e733a50ec752
CatsAreMLG <joshuadgon@gmail.com>
2019-07-02 18:29:14 -0500

    Change productions to production

--- knexfile.js
+++ knexfile.js
@@ -1,37 +1,37 @@
-require('dotenv').config();
+require('dotenv').config()
 
 module.exports = {
-	development: {
-		client: 'sqlite3',
-		connection: {
-			filename: './database/vendme.db3',
-		},
-		useNullAsDefault: true,
-		migrations: {
-			directory: './database/migrations',
-		},
-		seeds: {
-			directory: './database/seeds/development',
-		},
-	},
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/vendme.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds/development'
+    }
+  },
 
-	production: {
-		client: 'pg',
-		connection: process.env.DATABASE_URL,
-		pool: {
-			min: 2,
-			max: 10,
-		},
-		migrations: {
-			directory: './database/migrations',
-			tableName: 'vendmeprod_migrations',
-		},
-		seeds: {
-			directory: './database/seeds/productions',
-		},
-		useNullAsDefault: true,
-	},
-};
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'vendmeprod_migrations'
+    },
+    seeds: {
+      directory: './database/seeds/production'
+    },
+    useNullAsDefault: true
+  }
+}
 
 /* module.exports = {
 	development: {
