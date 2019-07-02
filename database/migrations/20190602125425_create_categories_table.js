f98abb5c6164960550cdb82d299b6beca943f906
CatsAreMLG <joshuadgon@gmail.com>
2019-07-02 16:45:29 -0500

    Test categories fix

--- database/migrations/20190602125425_create_categories_table.js
+++ database/migrations/20190602125425_create_categories_table.js
@@ -1,13 +1,13 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('categories', function(tbl) {
-    tbl.increments()
+// exports.up = function(knex, Promise) {
+//   return knex.schema.createTable('categories', function(tbl) {
+//     tbl.increments()
 
-    tbl.string('category_name', 255).notNullable()
+//     tbl.string('category_name', 255).notNullable()
 
-    tbl.string('category_desc', 255).notNullable()
-  })
-}
+//     tbl.string('category_desc', 255).notNullable()
+//   })
+// }
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('categories')
-}
+// exports.down = function(knex, Promise) {
+//   return knex.schema.dropTableIfExists('categories')
+// }
