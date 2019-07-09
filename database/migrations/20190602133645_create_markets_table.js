15cb6d360d2fc858313e7395aae61ce3378ede4b
Lavon Stalling <micah.shrak@gmail.com>
2019-07-08 20:19:27 -0700

    updated markets migration file and added new table for products vendors

--- database/migrations/20190602133645_create_markets_table.js
+++ database/migrations/20190602133645_create_markets_table.js
@@ -1,38 +1,40 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('markets', function(tbl) {
-    // PK
-    tbl.increments()
+	return knex.schema.createTable('markets', function(tbl) {
+		// PK
+		tbl.increments();
 
-    // Markets Info
-    tbl.string('market_name', 500).notNullable()
+		// Markets Info
+		tbl.string('market_name', 500).notNullable();
 
-    tbl.string('address', 255).notNullable()
+		tbl.string('address', 255).notNullable();
 
-    tbl.string('city', 255).notNullable()
+		tbl.string('city', 255).notNullable();
 
-    tbl.string('state', 2).notNullable()
+		tbl.string('state', 2).notNullable();
 
-    tbl.string('zip_code', 10).notNullable()
+		tbl.string('zip_code', 10).notNullable();
 
-    tbl.string('phone_num', 20).notNullable()
+		tbl.string('phone_num', 20).notNullable();
 
-    tbl.string('market_info', 1000).notNullable()
+		tbl.string('market_info', 1000).notNullable();
 
-    tbl.string('hours_open', 70)
+		tbl.string('hours_open', 70);
 
-    tbl.string('market_map_file', 2048)
+		tbl.string('market_map_file', 2048);
 
-    tbl.string('agreement_file', 2048)
+		tbl.string('agreement_file', 2048);
 
-    tbl.bigInteger('created_at').notNullable()
+		tbl.bigInteger('created_at').notNullable();
 
-    tbl
-      .integer('user_market')
-      .references('id')
-      .inTable('users')
-  })
-}
+		tbl
+			.integer('user_market')
+			.references('id')
+			.inTable('users')
+			.onDelete('CASCADE')
+			.onUpdate('CASCADE');
+	});
+};
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('markets')
-}
+	return knex.schema.dropTableIfExists('markets');
+};
