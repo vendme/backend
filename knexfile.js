000f838ffbc277177d8a0b351ab89e451db809d6
Kseniya Platonava <ksu2008@gmail.com>
2019-07-10 21:49:26 -0400

    testing

--- knexfile.js
+++ knexfile.js
@@ -25,44 +25,19 @@ module.exports = {
       directory: './database/seeds/production'
     },
     useNullAsDefault: true
-  }
-}
-
-/* module.exports = {
-	development: {
-		client: 'pg',
-		connection: 'postgres://localhost/<examples>',
-		migrations: {
-			directory: './db/migrations',
-		},
-		seeds: {
-			directory: './db/seeds/dev',
-		},
-		useNullAsDefault: true,
 	},
-
+	
 	test: {
-		client: 'pg',
-		connection: 'postgres://localhost/<examples_test>',
-		migrations: {
-			directory: './db/migrations',
-		},
-		seeds: {
-			directory: './db/seeds/test',
-		},
-		useNullAsDefault: true,
-	},
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.sqlite3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    }
 
-	production: {
-		client: 'pg',
-		connection: process.env.DATABASE_URL,
-		migrations: {
-			directory: './db/migrations',
-		},
-		seeds: {
-			directory: './db/seeds/production',
-		},
-		useNullAsDefault: true,
-	},
-};
- */
+}
