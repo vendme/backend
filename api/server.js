9f40d38f22b2975ba399870d695a659a426bde7c
brettmadrid <brettmadrid9@gmail.com>
2019-07-11 21:54:05 -0700

    Added Stripe Functionality to the Backend

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require("body-parser");
 
 const authRouter = require('../auth/auth-router.js')
 const usersRouter = require('../database/routes/users-router.js')
@@ -9,9 +10,12 @@ const vendorRouter = require('../database/routes/vendors-router.js')
 const marketRouter = require('../database/routes/markets-router.js')
 const stallRouter = require('../database/routes/stalls-router.js')
 const productsRouter = require('../database/routes/products-router.js')
+const paymentsRouter = require('../database/routes/payments');
 const server = express()
 const verifyToken = require('../auth/restricted-middleware')
 
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
@@ -22,7 +26,7 @@ server.use('/api/market', marketRouter)
 server.use('/api/stalls', stallRouter)
 server.use('/api/products', productsRouter)
 server.use('/auth', verifyToken, authRouter)
-//server.use('/', usersRouter)
+server.use('/api/payments', paymentsRouter);
 
 server.get('/', (req, res) => {
   res.send('I am the Vendme API/server. Up and running!')
