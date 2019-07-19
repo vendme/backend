bfd14d85ce2931e5ec715924e2323386c9659bf1
Lavon Stalling <micah.shrak@gmail.com>
2019-07-19 10:51:37 -0700

    Update user seed with profile pic

--- database/seeds/production/003-users_seeds.js
+++ database/seeds/production/003-users_seeds.js
@@ -1,29 +1,30 @@
-const faker = require('faker')
+const faker = require('faker');
 
 const createFakeUsers = () => {
-  let fakeUsers = []
-  const desiredFakeUsers = 10
-  for (let i = 0; i < desiredFakeUsers; i++) {
-    fakeUsers.push({
-      id: i + 1,
-      uid: faker.random.uuid(),
-      account_type: faker.random.number({ min: 1, max: 3 }),
-      email: faker.internet.email()
-    })
-  }
-  fakeUsers.push({
-    id: desiredFakeUsers + 1,
-    uid: 'GAkYhZmJf9QaUHAwTFQ9ql8NI833',
-    email: 'joshuadgon@gmail.com',
-    account_type: 1
-  })
-  return fakeUsers
-}
+	let fakeUsers = [];
+	const desiredFakeUsers = 10;
+	for (let i = 0; i < desiredFakeUsers; i++) {
+		fakeUsers.push({
+			id: i + 1,
+			uid: faker.random.uuid(),
+			account_type: faker.random.number({ min: 1, max: 3 }),
+			email: faker.internet.email(),
+			profile_pic: faker.image.avatar(),
+		});
+	}
+	fakeUsers.push({
+		id: desiredFakeUsers + 1,
+		uid: 'GAkYhZmJf9QaUHAwTFQ9ql8NI833',
+		email: 'joshuadgon@gmail.com',
+		account_type: 1,
+	});
+	return fakeUsers;
+};
 exports.seed = function(knex, Promise) {
-  return knex('users')
-    .del()
-    .then(function() {
-      // Inserts seed entries
-      return knex('users').insert(createFakeUsers())
-    })
-}
+	return knex('users')
+		.del()
+		.then(function() {
+			// Inserts seed entries
+			return knex('users').insert(createFakeUsers());
+		});
+};
