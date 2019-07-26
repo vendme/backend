4c0986a0f0af0df7f8e7fd5ec58f4fecb2a853b3
CatsAreMLG <joshuadgon@gmail.com>
2019-07-25 22:17:02 -0500

    Add contract_expires field to stalls table

--- database/migrations/20190602153814_create_stalls_table.js
+++ database/migrations/20190602153814_create_stalls_table.js
@@ -1,44 +1,46 @@
 exports.up = function(knex, Promise) {
-	return knex.schema.createTable('stalls', function(tbl) {
-		//PK
-		tbl.increments();
-		tbl.string('stall_name');
-		// FK
-		tbl
-			.integer('market_id')
-			.references('id')
-			.inTable('markets')
-			.onUpdate('CASCADE')
-			.onDelete('CASCADE');
+  return knex.schema.createTable('stalls', function(tbl) {
+    //PK
+    tbl.increments()
+    tbl.string('stall_name')
+    // FK
+    tbl
+      .integer('market_id')
+      .references('id')
+      .inTable('markets')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
 
-		tbl
-			.integer('vendor_id')
-			.references('id')
-			.inTable('vendors')
-			.onDelete('CASCADE');
+    tbl
+      .integer('vendor_id')
+      .references('id')
+      .inTable('vendors')
+      .onDelete('CASCADE')
 
-		tbl
-			.integer('category_id')
-			.references('id')
-			.inTable('categories')
-			.onDelete('CASCADE');
+    tbl
+      .integer('category_id')
+      .references('id')
+      .inTable('categories')
+      .onDelete('CASCADE')
 
-		tbl.integer('length').notNullable();
+    tbl.integer('length').notNullable()
 
-		tbl.integer('width').notNullable();
+    tbl.integer('width').notNullable()
 
-		tbl.boolean('availability');
+    tbl.boolean('availability')
 
-		tbl.string('description', 500);
+    tbl.string('description', 500)
 
-		tbl.string('stall_photo', 512);
+    tbl.string('stall_photo', 512)
 
-		tbl.decimal('stall_price', 5, 2).notNullable();
+    tbl.string('contract_expires', 512)
 
-		tbl.boolean('rent_message').defaultTo('F');
-	});
-};
+    tbl.decimal('stall_price', 5, 2).notNullable()
+
+    tbl.boolean('rent_message').defaultTo('F')
+  })
+}
 
 exports.down = function(knex, Promise) {
-	return knex.schema.dropTableIfExists('stalls');
-};
+  return knex.schema.dropTableIfExists('stalls')
+}
