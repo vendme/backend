2fe67a6f842352810a824841907277e52fa48518
CatsAreMLG <joshuadgon@gmail.com>
2019-07-27 23:11:53 -0500

    Add user profile pic seeds

--- database/seeds/development/006-users_seeds.js
+++ database/seeds/development/006-users_seeds.js
@@ -5,25 +5,27 @@ const createFakeUsers = () => {
   const desiredFakeUsers = 10
   for (let i = 0; i < desiredFakeUsers; i++) {
     fakeUsers.push({
-      id: i + 1,
       uid: faker.random.uuid(),
       account_type: faker.random.number({ min: 1, max: 3 }),
-      email: faker.internet.email()
+      email: faker.internet.email(),
+      profile_pic:
+        'https://res.cloudinary.com/vendme/image/upload/v1563242911/sample.jpg'
     })
   }
   fakeUsers.push({
-    id: desiredFakeUsers + 1,
     uid: 'GAkYhZmJf9QaUHAwTFQ9ql8NI833',
     email: 'joshuadgon@gmail.com',
-    account_type: 1
+    account_type: 1,
+    profile_pic:
+      'https://res.cloudinary.com/vendme/image/upload/v1564282027/qboxrgnocolnly3rkpex.jpg'
   })
   fakeUsers.push({
-    id: desiredFakeUsers + 2,
     uid: '8H6eZ2wfQ7ajLbnZMtWfF369CIE2',
     email: 'ash4741@gmail.com',
-    account_type: 1
+    account_type: 1,
+    profile_pic:
+      'https://res.cloudinary.com/vendme/image/upload/v1563242911/sample.jpg'
   })
-
   return fakeUsers
 }
 exports.seed = function(knex, Promise) {
