a7aa90189c8b82433c44a74d18412fcaebca6ac0
CatsAreMLG <joshuadgon@gmail.com>
2019-07-31 19:52:32 -0500

    Add markets longitudes and latitudes

--- database/migrations/20190602133645_create_markets_table.js
+++ database/migrations/20190602133645_create_markets_table.js
@@ -1,40 +1,44 @@
 exports.up = function(knex, Promise) {
-	return knex.schema.createTable('markets', function(tbl) {
-		// PK
-		tbl.increments();
+  return knex.schema.createTable('markets', function(tbl) {
+    // PK
+    tbl.increments()
 
-		// Markets Info
-		tbl.string('market_name', 500).notNullable();
+    // Markets Info
+    tbl.string('market_name', 500).notNullable()
 
-		tbl.string('address', 255).notNullable();
+    tbl.decimal('lon', [8, 8]).notNullable()
 
-		tbl.string('city', 255).notNullable();
+    tbl.decimal('lat', [8, 8]).notNullable()
 
-		tbl.string('state', 2).notNullable();
+    tbl.string('address', 255).notNullable()
 
-		tbl.string('zip_code', 10).notNullable();
+    tbl.string('city', 255).notNullable()
 
-		tbl.string('phone_num', 20).notNullable();
+    tbl.string('state', 2).notNullable()
 
-		tbl.string('market_info', 1000).notNullable();
+    tbl.string('zip_code', 10).notNullable()
 
-		tbl.string('hours_open', 70);
+    tbl.string('phone_num', 20).notNullable()
 
-		tbl.string('market_map_file', 2048);
+    tbl.string('market_info', 1000).notNullable()
 
-		tbl.string('agreement_file', 2048);
+    tbl.string('hours_open', 70)
 
-		tbl.bigInteger('created_at').notNullable();
+    tbl.string('market_map_file', 2048)
 
-		tbl
-			.integer('user_market')
-			.references('id')
-			.inTable('users')
-			.onDelete('SET NULL')
-			.onUpdate('SET NULL');
-	});
-};
+    tbl.string('agreement_file', 2048)
+
+    tbl.bigInteger('created_at').notNullable()
+
+    tbl
+      .integer('user_market')
+      .references('id')
+      .inTable('users')
+      .onDelete('SET NULL')
+      .onUpdate('SET NULL')
+  })
+}
 
 exports.down = function(knex, Promise) {
-	return knex.schema.dropTableIfExists('markets');
-};
+  return knex.schema.dropTableIfExists('markets')
+}
--- database/seeds/development/005-markets_seeds.js
+++ database/seeds/development/005-markets_seeds.js
@@ -1,37 +1,225 @@
-const faker = require('faker');
+const faker = require('faker')
 
 const createFakeMarkets = () => {
-	let fakeMarkets = [];
-	const desiredFakeMarkets = 10;
-	for (let i = 0; i < desiredFakeMarkets; i++) {
-		fakeMarkets.push({
-			//id: i + 1,
-			user_market: i + 1,
-			market_name: faker.company.companyName(),
-			address: faker.address.streetAddress(),
-			city: faker.address.city(),
-			state: faker.address.stateAbbr(),
-			zip_code: faker.address.zipCode(),
-			phone_num: faker.phone.phoneNumberFormat(),
-			hours_open: 'null;900,2100;930,2100;930,2100;930,2100;900,2200;900,2230',
-			market_info: faker.lorem.paragraph(),
-			market_map_file: faker.image.image(),
-			created_at: Date.parse(
-				faker.date.between(
-					new Date(Date.now() - 1000 * 60 * 60 * 24 * 20),
-					new Date(Date.now() - 1000 * 60 * 60 * 24 * 8)
-				)
-			),
-		});
-	}
-	return fakeMarkets;
-};
+  let fakeMarkets = [
+    {
+      user_market: 1,
+      market_name: 'Picapica Plaza',
+      address: '910 SE Military Dr',
+      lon: 29.35498,
+      lat: -98.48484,
+      city: 'San Antonio',
+      state: 'TX',
+      zip_code: '78214',
+      phone_num: '210-927-7422',
+      hours_open: '700,1900;null;700,1900;700,1900;700,1900;700,1900;700,1900',
+      market_info:
+        'Latin American-style mall boasting salons, fast-food stalls & diverse shops, plus amusement rides.',
+      market_map_file: faker.image.image(),
+      created_at: Date.parse(
+        faker.date.between(
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 20),
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 8)
+        )
+      )
+    },
+    {
+      user_market: 2,
+      market_name: 'Pan Am Plaza, Inc.',
+      address: '1419 Commercial Ave',
+      lon: 29.36668,
+      lat: -98.51768,
+      city: 'San Antonio',
+      state: 'TX',
+      zip_code: '78221',
+      phone_num: '210-922-6968',
+      hours_open: '900,1800;null;null;900,1800;900,1800;900,1800;900,1800',
+      market_info: faker.lorem.paragraph(),
+      market_map_file: faker.image.image(),
+      created_at: Date.parse(
+        faker.date.between(
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 20),
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 8)
+        )
+      )
+    },
+    {
+      user_market: 3,
+      market_name: 'The Mission Market Open Air Market',
+      address: '207 W Chavaneaux Rd',
+      lon: 29.32069,
+      lat: -98.50264,
+      city: 'San Antonio',
+      state: 'TX',
+      zip_code: '78221',
+      phone_num: '210-923-8131',
+      hours_open: '700,1900;null;null;700,1900;null;null;700,1900',
+      market_info:
+        'Jumbo flea market with 2000+ vendors selling Mexican candy, Western wear, plants, tools & more.',
+      market_map_file: faker.image.image(),
+      created_at: Date.parse(
+        faker.date.between(
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 20),
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 8)
+        )
+      )
+    },
+    {
+      user_market: 4,
+      market_name: 'The Mission Market Open Air Market #2',
+      address: '208 W Chavaneaux Rd',
+      lon: 29.32069,
+      lat: -98.50264,
+      city: 'San Antonio',
+      state: 'TX',
+      zip_code: '78221',
+      phone_num: '210-923-8132',
+      hours_open: '700,1900;null;null;700,1900;null;null;700,1900',
+      market_info:
+        'Jumbo flea market with 2001+ vendors selling Mexican candy, Western wear, plants, tools & more.',
+      market_map_file: faker.image.image(),
+      created_at: Date.parse(
+        faker.date.between(
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 20),
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 8)
+        )
+      )
+    },
+    {
+      user_market: 5,
+      market_name: 'Mi Mercado Flea Market and Ballroom',
+      address: '227 New Laredo Hwy',
+      lon: 29.37301,
+      lat: -98.53875,
+      city: 'San Antonio',
+      state: 'TX',
+      zip_code: '78211',
+      phone_num: '210-922-7500',
+      hours_open: '700,1600;null;null;700,1500;700,1900;700,1500;700,1600',
+      market_info:
+        'Jumbo flea market with 2000+ vendors selling Mexican candy, Western wear, plants, tools & more.',
+      market_map_file: faker.image.image(),
+      created_at: Date.parse(
+        faker.date.between(
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 20),
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 8)
+        )
+      )
+    },
+    {
+      user_market: 6,
+      market_name: 'Flea Market',
+      address: '115 St Louis Ave',
+      lon: 29.43218,
+      lat: -98.56354,
+      city: 'San Antonio',
+      state: 'TX',
+      zip_code: '78237',
+      phone_num: faker.phone.phoneNumberFormat(),
+      hours_open: '700,1600;null;null;700,1500;700,1900;700,1500;700,1600',
+      market_info:
+        'Jumbo flea market with 2000+ vendors selling Mexican candy, Western wear, plants, tools & more.',
+      market_map_file: faker.image.image(),
+      created_at: Date.parse(
+        faker.date.between(
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 20),
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 8)
+        )
+      )
+    },
+    {
+      user_market: 7,
+      market_name: 'Highway 90 Flea Market/Alamo MarketPlace',
+      address: '7171 W US Hwy 90',
+      lon: 29.40321,
+      lat: -98.63198,
+      city: 'San Antonio',
+      state: 'TX',
+      zip_code: '78227',
+      phone_num: '210-645-7170',
+      hours_open: '1000,1800;null;null;null;null;1100,1800;1000,1800',
+      market_info:
+        'Huge, air-conditioned locale boasting 100s of vendors offering new & used items, services & food.',
+      market_map_file: faker.image.image(),
+      created_at: Date.parse(
+        faker.date.between(
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 20),
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 8)
+        )
+      )
+    },
+    {
+      user_market: 8,
+      market_name: 'Blanco Centre Bazaar',
+      address: '1705 Blanco Rd',
+      lon: 29.46184,
+      lat: -98.50808,
+      city: 'San Antonio',
+      state: 'TX',
+      zip_code: '78212',
+      phone_num: faker.phone.phoneNumberFormat(),
+      hours_open: '1000,1800;null;null;null;null;1100,1800;1000,1800',
+      market_info:
+        'Huge, air-conditioned locale boasting 100s of vendors offering new & used items, services & food.',
+      market_map_file: faker.image.image(),
+      created_at: Date.parse(
+        faker.date.between(
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 20),
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 8)
+        )
+      )
+    },
+    {
+      user_market: 9,
+      market_name: 'Bandera Flea Market',
+      address: '4002, 1331 Bandera Rd',
+      lon: 29.46415,
+      lat: -98.56605,
+      city: 'San Antonio',
+      state: 'TX',
+      zip_code: '78228',
+      phone_num: '210-433-2004',
+      hours_open: '1000,1900;null;null;null;null;1000,1800;1000,1900',
+      market_info:
+        'Indoor flea market selling second-hand items & clothing along with with food & live music.',
+      market_map_file: faker.image.image(),
+      created_at: Date.parse(
+        faker.date.between(
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 20),
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 8)
+        )
+      )
+    },
+    {
+      user_market: 10,
+      market_name: 'Pearl Farmers Market',
+      address: '312 Pearl Pkwy',
+      lon: 29.44212,
+      lat: -98.48021,
+      city: 'San Antonio',
+      state: 'TX',
+      zip_code: '78215',
+      phone_num: '210-212-7260',
+      hours_open: '1000,1400;null;null;null;null;null;900,1300',
+      market_info:
+        'Indoor flea market selling second-hand items & clothing along with with food & live music.',
+      market_map_file: faker.image.image(),
+      created_at: Date.parse(
+        faker.date.between(
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 20),
+          new Date(Date.now() - 1000 * 60 * 60 * 24 * 8)
+        )
+      )
+    }
+  ]
+  return fakeMarkets
+}
 
 exports.seed = function(knex, Promise) {
-	return knex('markets')
-		.del()
-		.then(function() {
-			// Inserts seed entries
-			return knex('markets').insert(createFakeMarkets());
-		});
-};
+  return knex('markets')
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex('markets').insert(createFakeMarkets())
+    })
+}
